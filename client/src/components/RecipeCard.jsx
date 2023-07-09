3fb55cd753a725c3201b580efb5b3e24967ffd62
Adam Buffett <adam_buffett@outlook.com>
2023-07-09 17:01:45 -0400

    Create RecipeDetail page

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const RecipeCard = ({ recipe }) => {
   return (
-    <Link to={`/recipe/${recipe._id}`}>
+    <Link to={`/recipes/${recipe._id}`}>
       <div>
         <h1>{recipe.name}</h1>
         <h3>Category: {recipe.category}</h3>
--- client/src/pages/RecipeDetail.jsx
+++ client/src/pages/RecipeDetail.jsx
@@ -1,5 +1,26 @@
-const RecipeDetail = () => {
-  return <div></div>
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+const RecipeDetail = ({ recipes }) => {
+  const [recipe, setRecipe] = useState(null)
+
+  const { recipeId } = useParams()
+
+  useEffect(() => {
+    const selectRecipe = () => {
+      let selectRecipe = recipes.find((recipe) => {
+        return recipe._id === recipeId
+      })
+      setRecipe(selectRecipe)
+    }
+    selectRecipe()
+  }, [])
+
+  return (
+    <div>
+      <h1>{recipe.name}</h1>
+    </div>
+  )
 }
 
 export default RecipeDetail
