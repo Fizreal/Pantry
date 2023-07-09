1690c20cdb487fed3c7891b1a51c479d75118806
Adam Buffett <adam_buffett@outlook.com>
2023-07-09 12:23:29 -0400

    Set up App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,55 @@
 import './App.css'
 import './index.css'
 import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { CheckSession } from './services/Auth'
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Recipes from './pages/Recipes'
+import RecipeDetail from './pages/RecipeDetails'
+import Groceries from './pages/Groceries'
+import GroceryDetail from './pages/GroceryDetail'
 
 const App = () => {
-  return <div className="App"></div>
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    let user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <header>
+        <Nav user={user} handleLogOut={handleLogOut} />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/recipes/:recipeId" element={<RecipeDetail />} />
+          <Route path="/groceries" element={<Groceries />} />
+          <Route path="/groceries/:groceryId" element={<GroceryDetail />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
